25f4693f832b809ca5b3a27d6f7894d1fe95aea3
ValeryMakush <kn1b22.makush@kpnu.edu.ua>
2026-03-08 11:50:13 +0200

    enhance footer layout and add GitHub credit link; include RouterLink in rooms component

--- src/app/pages/rooms/rooms.ts
+++ src/app/pages/rooms/rooms.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-rooms',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './rooms.html',
   styleUrl: './rooms.css'
 })
